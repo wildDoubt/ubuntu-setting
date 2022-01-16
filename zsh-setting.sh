680934bf5d5d58813592ed86b4ce9d4aff352d26
Wonyeong Choi <coma01155@gmail.com>
2022-01-17 01:03:59 +0900

    initial commit

---
+++ zsh-setting.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt-get update
+
+# zsh 설치
+sudo apt install zsh -y && chsh -s `which zsh`
+
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+# zsh 플러그인 설치
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
+
+# zsh 터미널 설정 복사
+cp ./.zshrc ~/.zshrc
+
+sudo su
+sudo echo 'wonyeong ALL=NOPASSWD: ALL' >> /etc/sudoers
+sudo su - wonyeong
